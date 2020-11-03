08b720a3d86ddf5925d3d872f61f3a442c752cec
Pivithuru Jayakody <piveedj@gmail.com>
2020-11-04 00:25:12 +0530

    Handle Custom Key Creation

--- app.js
+++ app.js
@@ -33,7 +33,10 @@ class App {
     } else {
       this.shouldUseDefaultKeys = await this.selectUseDefaultKeys();
       if (this.shouldUseDefaultKeys === 'No. Generate a new pair.') {
-        this.rsaService.generateKeyPair();
+        const keySize = await this.selectKeySize();
+        const encryptionScheme = await this.selectEncryptionScheme();
+        const signingScheme = await this.selectSigningScheme();
+        this.rsaService.generateKeyPair({ keySize, encryptionScheme, signingScheme });
         console.log('\nPublic Key:');
         console.log(this.rsaService.publicKey);
         console.log('\nPrivate Key:');
@@ -98,6 +101,77 @@ class App {
     });
     return io.run()
       .then(answer => answer)
+      .catch(console.error);
+  }
+
+  selectKeySize() {
+    const io = new Select({
+      name: 'keySize',
+      message: 'Select a bit size for the keys.',
+      choices: [
+        '512',
+        '1024',
+        '2048',
+        '4096',
+      ]
+    });
+    return io.run()
+      .then(answer => parseInt(answer))
+      .catch(console.error);
+  }
+
+  selectEncryptionScheme() {
+    const io = new Select({
+      name: 'keyEncryptionScheme',
+      message: 'Select Encryption Scheme.',
+      choices: [
+        'PKCS1 with OAEP',
+        'PKCS1'
+      ]
+    });
+    return io.run()
+      .then(answer => {
+        switch (answer) {
+          case 'PKCS1 with OAEO':
+            return 'pkcs1_oaep';
+          case 'PKCS1':
+            return 'pkcs1';
+          default:
+            return null;
+        }
+      })
+      .catch(console.error);
+  }
+
+  selectSigningScheme() {
+    const io = new Select({
+      name: 'keySigningScheme',
+      message: 'Select Signing Scheme.',
+      choices: [
+        'PKCS1-SHA256',
+        'PSS-SHA1',
+        'PSS-SHA256',
+        'PSS-SHA512',
+        'PSS-MD5',
+      ]
+    });
+    return io.run()
+      .then(answer => {
+        switch (answer) {
+          case 'PKCS1-SHA256':
+            return 'pkcs1-sha256';
+          case 'PSS-SHA1':
+            return 'pss';
+          case 'PSS-SHA256':
+            return 'pss-sha256';
+          case 'PSS-SHA512':
+            return 'pss-sha512';
+          case 'PSS-MD5':
+            return 'pss-md5';
+          default:
+            return null;
+        }
+      })
       .catch(console.error);
   }
 
