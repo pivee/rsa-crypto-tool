876fef7334776a9679212706a00353724b676443
Pivithuru Jayakody <piveedj@gmail.com>
2020-11-03 22:17:16 +0530

    Create Basic Functionality

--- app.js
+++ app.js
@@ -1,16 +1,63 @@
 const chalk = require('chalk');
-const { Select } = require('enquirer');
+const { Toggle, Input, Select } = require('enquirer');
+const { NodeRSAService } = require('./services/node-rsa.service');
 
 class App {
   name = 'RSA Testing Tool';
+  rsaService = new NodeRSAService({ useExistingKeyPair: true });
+  hasCustomKeys = false;
+  shouldUseDefaultKeys = true;
+  testType = 'Encryption';
+  testString = '';
 
-  launch() {
-    console.log(`Launching ${this.name} 🚀`);
-    const testType = await this.selectEncryptionOrDecryption();
+
+  async launch() {
+    console.log(chalk.yellow(`Launching ${this.name}... 🚀🚀🚀\n`));
+    this.hasCustomKeys = await this.toggleHaveKeys();
+    if (this.hasCustomKeys) {
+      console.log('What? You do???');
+      process.exit();
+    } else {
+      this.shouldUseDefaultKeys = await this.selectUseDefaultKeys();
+    }
+    if (this.shouldUseDefaultKeys === 'No. Generate a new pair.') {
+      this.rsaService.generateKeyPair();
+      console.log('\nPublic Key:');
+      console.log(this.rsaService.publicKey);
+      console.log('\nPrivate Key:');
+      console.log(this.rsaService.privateKey, '\n');
+    }
+    this.testType = await this.selectEncryptionOrDecryption();
+    switch (this.testType) {
+      case 'Encryption':
+        this.testString = await this.inputTestString('encrypt');
+        const cipher = this.rsaService.encrypt(this.testString);
+        console.log(chalk.yellow(cipher));
+        break;
+      case 'Decryption':
+        this.testString = await this.inputTestString('decrypt');
+        const plainText = this.rsaService.decrypt(this.testString);
+        console.log(chalk.blue(plainText));
+        break;
+      default:
+        console.log('Something went wrong');
+        process.exit();
+    }
+  }
+
+  toggleHaveKeys() {
+    const io = new Toggle({
+      message: 'Do you have custom a key pair?',
+      enabled: 'Yes',
+      disabled: 'No',
+    });
+    return io.run()
+      .then(answer => answer)
+      .catch(console.error);
   }
 
   selectEncryptionOrDecryption() {
-    const prompt = new Select({
+    const io = new Select({
       name: 'testType',
       message: 'Select Test Type',
       choices: [
@@ -18,7 +65,31 @@ class App {
         'Encryption',
       ]
     });
-    return prompt.run()
+    return io.run()
+      .then(answer => answer)
+      .catch(console.error);
+  }
+
+  selectUseDefaultKeys() {
+    const io = new Select({
+      name: 'keyType',
+      message: 'Do you want to use the default keys?',
+      choices: [
+        'Yes. Use the default keys.',
+        'No. Generate a new pair.'
+      ]
+    });
+    return io.run()
+      .then(answer => answer)
+      .catch(console.error);
+  }
+
+  inputTestString(cryptoFunction) {
+    const io = new Input({
+      message: `Enter the string to ${cryptoFunction}\n`,
+      initial: ''
+    });
+    return io.run()
       .then(answer => answer)
       .catch(console.error);
   }
