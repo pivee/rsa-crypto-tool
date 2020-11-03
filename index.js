94e4f9b1f0521b834fc868a56c9b1f735454651c
Pivithuru Jayakody <piveedj@gmail.com>
2020-11-04 01:55:22 +0530

    Create directory for Keys at initialization

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { App } = require('./app');
 
 const initializers = new Promise((resolve, reject) => {
   if (!fs.existsSync('./exports')) fs.mkdirSync('./exports');
+  if (!fs.existsSync('./exports/keys')) fs.mkdirSync('./exports/keys');
   if (!fs.existsSync('./exports/logs')) fs.mkdirSync('./exports/logs');
   resolve();
 });
